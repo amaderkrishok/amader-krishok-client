c2106b58c66097b8f64309171638bd633e4b906b
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-08-03 22:25:02 +0600

    Bug Fixed

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 		rules: {
 			userAgent: '*',
 			allow: '/',
-			disallow: ['/admin/', '/api/'],
+			disallow: ['/admin/', '/api/','/user/', '/vendor/', '/session/'],
 		},
 		sitemap: 'https://www.amaderkrishok.com/sitemap.xml',
 	};
